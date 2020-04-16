0f40508ef3eaba84f415091d7162748f3c1ed261
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-04-16 15:54:36 -0300

    Relacionamento nos models

--- src/database/migrations/1586995826896-CreateAppointments.ts
+++ src/database/migrations/1586995826896-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1586995826896
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1587058562731-CreateUsers.ts
+++ src/database/migrations/1587058562731-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587058562731 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
