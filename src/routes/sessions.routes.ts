2e47c56de3cb2825b1e827b5f9b442a7d2591354
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-04-17 08:13:54 -0300

    validando credenciais

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express'
+
+import AuthenticateUserService from '../services/AuthenticateUserService'
+
+const sessionsRouter = Router()
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body
+
+    const authenticateUser = new AuthenticateUserService()
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    })
+
+    delete user.password
+
+    return response.json({ user })
+  } catch (err) {
+    return response.status(400).json({ error: err.message })
+  }
+})
+
+export default sessionsRouter
