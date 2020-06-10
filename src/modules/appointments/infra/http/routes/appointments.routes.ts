1879c6c81ad7fdbccc2222fd0168695273fb3ed8
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-10 20:40:00 -0300

    Dependency Inversion Principle

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,29 +1,28 @@
 import { Router } from 'express'
-import { getCustomRepository } from 'typeorm'
 import { parseISO } from 'date-fns'
 
-import AppointmentRepository from '@modules/appointments/repositories/AppoitmentsRepository'
+import AppointmentRepository from '@modules/appointments/infra/typeorm/repositories/AppoitmentsRepository'
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 
 const appointmentsRouter = Router()
+const appointmentsRepository = new AppointmentRepository()
 
 appointmentsRouter.use(ensureAuthenticated)
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentRepository)
-  const appointments = await appointmentsRepository.find()
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsRepository.find()
 
-  return response.json(appointments)
-})
+//   return response.json(appointments)
+// })
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body
 
   const parsedDate = parseISO(date)
 
-  const createAppointment = new CreateAppointmentService()
+  const createAppointment = new CreateAppointmentService(appointmentsRepository)
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,23 +1,22 @@
 import { startOfHour } from 'date-fns'
-import { getCustomRepository } from 'typeorm'
 
 import AppError from '@shared/errors/AppError'
 
 import Appointment from '../infra/typeorm/entities/Appointment'
-import AppointmentsRepository from '../repositories/AppoitmentsRepository'
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
-interface Request {
+interface IRequest {
   provider_id: string
   date: Date
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date)
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     )
 
@@ -25,11 +24,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked')
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     })
-
 
     return appointment
   }
