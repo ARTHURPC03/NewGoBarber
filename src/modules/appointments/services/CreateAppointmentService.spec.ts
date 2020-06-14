67336d82f0e4e0aa52b26feb8353258c85041bfd
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-14 05:38:47 -0300

    Testes de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,14 +1,16 @@
+import AppError from '@shared/errors/AppError'
+
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentRepository'
 import CreateAppointmentService from './CreateAppointmentService'
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const fakeAppointmentRepository = new FakeAppointmentRepository()
-    const CreateAppointment = new CreateAppointmentService(
+    const createAppointment = new CreateAppointmentService(
       fakeAppointmentRepository,
     )
 
-    const appointment = await CreateAppointment.execute({
+    const appointment = await createAppointment.execute({
       date: new Date(),
       provider_id: '123123',
     })
@@ -17,7 +19,24 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123')
   })
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3)
-  // })
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository()
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    )
+
+    const appointmentDate = new Date(2020, 6, 15, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    })
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
 })
