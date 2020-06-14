d38aefe47f64d239197eadc22b4c1a96e8e00bae
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-14 06:34:39 -0300

    Testando criação de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError'
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import CreateUserService from './CreateUserService'
+
+describe('CreateUser', () => {
+  it('should be able to create a user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const createUser = new CreateUserService(fakeUsersRepository)
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user).toHaveProperty('id')
+  })
+
+  it('should not be able to create a new user with same email form another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const createUser = new CreateUserService(fakeUsersRepository)
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError)
+  })
+})
