920554bfd348ad2c2e5e614066dd61a9a04ec33d
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-23 07:04:34 -0300

    Recuperação de senha

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -44,7 +44,7 @@ describe('AuthenticateUser', () => {
       fakeHashProvider,
     )
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: '123456',
@@ -72,7 +72,7 @@ describe('AuthenticateUser', () => {
       password: '123456',
     })
 
-    expect(
+    await expect(
       authenticateUser.execute({
         email: 'johndoe@example.com',
         password: 'wrong-password',
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,8 +1,9 @@
 import { injectable, inject } from 'tsyringe'
 
-// import AppError from '@shared/errors/AppError'
+import AppError from '@shared/errors/AppError'
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 import IUsersRepository from '../repositories/IUsersRepository'
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
 
 // import User from '../infra/typeorm/entities/User'
 
@@ -18,9 +19,20 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new AppError('User does not exists')
+    }
+
+    await this.userTokensRepository.generate(user.id)
+
     this.mailProvider.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
