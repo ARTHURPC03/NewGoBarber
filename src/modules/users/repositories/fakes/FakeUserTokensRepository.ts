cfcc555ffd6d50cbd2dd8399bebff5c4088b013c
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-23 09:58:34 -0300

    Reset de senha

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -1,6 +1,6 @@
 import { uuid } from 'uuidv4'
 
-import IUsersTokenRepository from '@modules/users/repositories/IUsersTokensRepository'
+import IUsersTokenRepository from '@modules/users/repositories/IUserTokensRepository'
 
 import UserToken from '../../infra/typeorm/entities/UserToken'
 
@@ -17,6 +17,14 @@ class FakeUserTokensRepository implements IUsersTokenRepository {
     })
 
     this.userTokens.push(userToken)
+
+    return userToken
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    )
 
     return userToken
   }
