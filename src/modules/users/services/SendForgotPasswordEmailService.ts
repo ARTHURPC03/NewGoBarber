916c606e70732ce27f144f54d1e382471e258a41
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-24 07:03:53 -0300

    Emails em desenvolvimento

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,11 +31,11 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists')
     }
 
-    await this.userTokensRepository.generate(user.id)
+    const { token } = await this.userTokensRepository.generate(user.id)
 
-    this.mailProvider.sendMail(
+    await this.mailProvider.sendMail(
       email,
-      'Pedido de recuperação de senha recebido.',
+      `Pedido de recuperação de senha recebido: ${token}`,
     )
   }
 }
