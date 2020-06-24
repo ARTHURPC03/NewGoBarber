fd34a059b744093934a46891a12fab0cb2d3b88c
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-24 06:06:01 -0300

    Salvando tokens no banco

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express'
+
+import { container } from 'tsyringe'
+
+import ResetPasswordService from '@modules/users/services/ResetPasswordService'
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body
+
+    const resetPassword = container.resolve(ResetPasswordService)
+
+    await resetPassword.execute({
+      password,
+      token,
+    })
+
+    return response.status(204).json()
+  }
+}
