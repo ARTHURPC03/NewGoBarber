358362e7e7b8136d6e69a22d9e8cf03b1e159f49
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-27 10:31:03 -0300

    Listagem de prestadores

---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from 'tsyringe'
+
+import AppError from '@shared/errors/AppError'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository'
+
+import User from '@modules/users/infra/typeorm/entities/User'
+
+interface IRequest {
+  user_id: string
+}
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    })
+
+    if (!users) {
+      throw new AppError('User not found')
+    }
+    return users
+  }
+}
+
+export default ListProvidersService
