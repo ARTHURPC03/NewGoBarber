7008d9452b5d46b66d2d54ec831243ac281e0466
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-29 06:08:53 -0300

    excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe'
 
-import { getHours } from 'date-fns'
+import { getHours, isAfter } from 'date-fns'
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
@@ -45,14 +45,18 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart,
     )
 
+    const currentDate = new Date(Date.now())
+
     const availability = eachHourArray.map(hour => {
       const hasAppintmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       )
 
+      const compareDate = new Date(year, month - 1, day, hour)
+
       return {
         hour,
-        available: !hasAppintmentInHour,
+        available: !hasAppintmentInHour && isAfter(compareDate, currentDate),
       }
     })
 
