f48536546c90c880dea1cb1f3632110d7d02eeae
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-06-30 21:22:06 -0300

    Validando dados

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import multer from 'multer'
 import uploadConfig from '@config/upload'
+import { celebrate, Segments, Joi } from 'celebrate'
 
 import UsersController from '../controllers/UsersController'
 import UserAvatarController from '../controllers/UserAvatarController'
@@ -12,7 +13,17 @@ const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
 const upload = multer(uploadConfig)
 
-usersRouter.post('/', usersController.create)
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+)
 
 usersRouter.patch(
   '/avatar',
