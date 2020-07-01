e5b22aad9b11c150a663ff7ac2bc509d2a36565c
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-01 06:22:38 -0300

    Emails pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses'
+
+  defaults: {
+    from: {
+      email: string
+      name: string
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'arthurpc@rocketseat.com.br',
+      name: 'ARTHUR PC',
+    },
+  },
+} as IMailConfig
