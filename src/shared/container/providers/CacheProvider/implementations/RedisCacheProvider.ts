d79038ccf497a79cce3b7fe1b4a62d0d588355a2
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-02 09:15:09 -0300

    Cache lista de providers

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -10,13 +10,19 @@ export default class RedisCacheProvider implements ICacheProvider {
   }
 
   public async save(key: string, value: string): Promise<void> {
-    await this.client.set(key, value)
+    await this.client.set(key, JSON.stringify(value))
   }
 
-  public async recover(key: string): Promise<string | null> {
+  public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key)
 
-    return data
+    if (!data) {
+      return null
+    }
+
+    const parsedDate = JSON.parse(data) as T
+
+    return parsedDate
   }
 
   public async invalidate(key: string): Promise<void> {}
