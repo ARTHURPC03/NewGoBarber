2194d2bf08fb98d29d341ebfad61335f3fb5855c
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-02 12:36:51 -0300

    Cache de agendamentos

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express'
-import { parseISO } from 'date-fns'
 import { container } from 'tsyringe'
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService'
@@ -10,12 +9,10 @@ export default class AppointmentsController {
 
     const { provider_id, date } = request.body
 
-    const parsedDate = parseISO(date)
-
     const createAppointment = container.resolve(CreateAppointmentService)
 
     const appointment = await createAppointment.execute({
-      date: parsedDate,
+      date,
       provider_id,
       user_id,
     })
--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,20 +27,24 @@ class ListProviderAppointmentsService {
     month,
     day,
   }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd')
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`
 
-    console.log(cacheData)
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey)
 
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
-      {
-        provider_id,
-        year,
-        month,
-        day,
-      },
-    )
+    if (!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+        {
+          provider_id,
+          year,
+          month,
+          day,
+        },
+      )
 
-    // await this.cacheProvider.save('asd', 'asd')
+      console.log('Buscou do banco')
+
+      await this.cacheProvider.save(cacheKey, appointments)
+    }
 
     return appointments
   }
