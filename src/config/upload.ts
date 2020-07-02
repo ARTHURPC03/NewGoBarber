f5f7d1af790f5bfe9c1e9485e13c5ed5aeeb8331
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-02 05:55:17 -0300

    Upload de arquivos para o Amazon S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,49 @@
 import path from 'path'
 import crypto from 'crypto'
-import multer from 'multer'
+import multer, { StorageEngine } from 'multer'
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp')
 
+interface IUploadConfig {
+  driver: 's3' | 'disk'
+
+  tmpFolder: string
+  uploadsFolder: string
+
+  multer: {
+    storage: StorageEngine
+  }
+
+  config: {
+    disk: {}
+    aws: {
+      bucket: string
+    }
+  }
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHase = crypto.randomBytes(10).toString('HEX')
-      const fileName = `${fileHase}-${file.originalname}`
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHase = crypto.randomBytes(10).toString('HEX')
+        const fileName = `${fileHase}-${file.originalname}`
 
-      return callback(null, fileName)
+        return callback(null, fileName)
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-}
+  },
+} as IUploadConfig
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,8 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated'
 const usersRouter = Router()
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
-const upload = multer(uploadConfig)
+
+const upload = multer(uploadConfig.multer)
 
 usersRouter.post(
   '/',
