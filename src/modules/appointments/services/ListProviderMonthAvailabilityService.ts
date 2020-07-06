38098c66dbb68e9e2b4278f85ff96af2e4f214e7
arthurpc03 <arthurpimetelcordeiro@gmail.com>
2020-07-06 05:07:42 -0300

    Dias indisponíveis no mês

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe'
 
-import { getDaysInMonth, getDate } from 'date-fns'
+import { getDaysInMonth, getDate, isAfter } from 'date-fns'
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 
@@ -45,13 +45,16 @@ class ListProviderMonthAvailabilityService {
     )
 
     const availability = eachDayArray.map(day => {
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59)
+
       const appointmentsInDay = appointments.filter(appointment => {
         return getDate(appointment.date) === day
       })
 
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       }
     })
 
